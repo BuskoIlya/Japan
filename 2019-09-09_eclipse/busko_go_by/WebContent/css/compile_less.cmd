2a73fd5a33e4f5776e1ac316d229a46b4fb05051
Ilya Busko <busko@synopsys.com>
2019-09-13 15:39:39 +0300

    [WEB][ECLIPSE] Move prj to eclipse-web

---
+++ 2019-09-09_eclipse/busko_go_by/WebContent/css/compile_less.cmd
@@ -0,0 +1,5 @@
+echo Compiling CSS...   
+call lessc index.less index.css
+call lessc common.less common.css
+call lessc menu.less menu.css
+
---
+++ 2019-09-09_eclipse/busko_go_by/WebContent/old/less/compile_less.cmd
@@ -0,0 +1,5 @@
+echo Compiling CSS...   
+call lessc index.less index.css
+call lessc common.less common.css
+call lessc t_club.less t_club.css
+
