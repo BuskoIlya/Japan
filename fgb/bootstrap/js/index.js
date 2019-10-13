18ee48e52de8c87d3a3898242c93ff4cd497e741
BuskoIlya <trassae95st@mail.ru>
2019-10-13 13:06:14 +0300

    [WEWB] Main page is beautiful

---
+++ fgb/bootstrap/js/index.js
@@ -0,0 +1,11 @@
+function html_in_div(html_from, div_to) {
+    $(div_to).load(html_from);
+}
+
+function clear_div(div) {
+    $(div).empty();
+}
+
+/*function create_news(img, title, text, div_to) {
+    $("#b_news_template").
+}*/
