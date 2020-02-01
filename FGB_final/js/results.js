b6482dcc979ee4eddd67c95e63bd73772138de5d
BuskoIlya <trassae95st@mail.ru>
2020-02-01 20:43:31 +0300

    [WEB] Improvements

--- FGB_final/js/results.js
+++ FGB_final/js/results.js
@@ -412,6 +412,38 @@ $.get('results/2019_12_20_1.txt', function (data) {
     );
 });
 
+class Player_2019_12_20_2 {
+    constructor(place, player, city, rank,
+                game1, game2, game3, game4, total, mms, sos, sdos) {
+        this.place = place;
+        this.player = player
+        this.city = city;
+        this.rank = rank;
+        this.game1 = game1;
+        this.game2 = game2;
+        this.game3 = game3;
+        this.game4 = game4;
+        this.total = total;
+        this.mms = mms;
+        this.sos = sos;
+        this.sdos = sdos;
+    }
+}
+
+var results_2019_12_20_2 = [];
+
+$.get('results/2019_12_20_2.txt', function (data) {
+    let lines = data.split("\n");
+    $.each(lines, function (n, elem) {
+            let f = elem.split(",");
+            let player = new Player_2019_12_20_2(
+                f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7],
+                f[8], f[9], f[10], f[11]);
+            results_2019_12_20_2.push(player);
+        }
+    );
+});
+
 // ----------------------
 // 2020-01-24
 // ----------------------
